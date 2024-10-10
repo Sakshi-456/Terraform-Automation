c150d3b61c02b97f5183f09b25ed6df59ad3a25d
sakshi <sakshikhandar97@gmail.com>
2024-10-10 14:04:13 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-project-terraform-batch-25"
+    bucket = "mydev-project-terraform"
     key = "main"
-    region = "us-east-1"
+    region = "ap-south-1"
     dynamodb_table = "my-dynamodb-table"
   }
 }
